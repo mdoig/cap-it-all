250631921d87bd99998ba8666e5909876aa686c9
Rocky Racoon <mdoig5247@gmail.com>
2020-05-22 23:06:17 -0500

    appending with csvdata and task; fix npm vulnerability with package-lock no-no

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,14 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
+const csvdata = require('csvdata');
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('task', {
+    log(message) {
+      csvdata.write('cypress/fixtures/huckTest2.csv', message, { append: true, header: 'brandName,value1,value2' });
+      return null
+    }
+  });
 }
